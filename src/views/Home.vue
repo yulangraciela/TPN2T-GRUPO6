c2cf1fb8648887540fe8798baf288a166093f859
graciela yulan <yulan.it.graciela@gmail.com>
2021-06-07 18:38:24 -0300

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,37 @@
 <template>
-  <div :style="myStyle" class="Home">
-   
-    <img alt="Vue logo" src="../assets/logo1.svg" />
+  <b-container fluid="sm">
+    <div :style="myStyle" class="Home">
+      <img alt="Vue logo" src="../assets/logo1.svg" />
 
-    <router-link :to="{ name: 'MisPracticas' }">ir a mis Practicas</router-link>
-    <carousel />
-    <MisPracticas/>
-  </div>
+      <router-link :to="{ name: 'MisPracticas' }"
+        >ir a mis Practicas</router-link
+      >
+      <carousel />
+      <MisPracticas />
+      <Titulo texto="La academia de música en casa"/>
+    </div>
+  </b-container>
 </template>
 
 <script>
 // @ is an alias to /src
 import carousel from "../components/carousel.vue";
 import MisPracticas from "../components/MisPracticas.vue";
+import Titulo from "../components/Titulo.vue";
 export default {
   name: "Home",
-  data(){
-       return{
-         myStyle:{
-            backgroundColor:"#16a085" 
-         }
-            }
-  },
-  components:{
 
+  data() {
+    return {
+      myStyle: {
+        backgroundColor: "#16a085",
+      },
+    };
+  },
+  components: {
     carousel,
     MisPracticas,
-  
-  }
-  
+    Titulo,
+  },
 };
 </script>
