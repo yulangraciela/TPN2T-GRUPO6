2711c4e7c8146046c89862a122e4deeb7d1aee53
graciela yulan <yulan.it.graciela@gmail.com>
2021-06-07 18:37:05 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
-<b-container>  
-      <b-nav card-header pills>
-        <!-- <b-nav-item>'s with child routes. Note the trailing slash on the first <b-nav-item> -->
-        <b-nav-item to="/" exact exact-active-class="active">Home</b-nav-item>
-        <b-nav-item :to="{ name: 'About' }" exact exact-active-class="active">About</b-nav-item>
-        <b-nav-item :to="{ name: 'Login' }" exact exact-active-class="active">Login</b-nav-item>
-        <b-nav-item :to="{ name: 'MisPracticas' }" exact exact-active-class="active">Mis Practicas</b-nav-item>
-        <b-nav-item :to="{ name: 'LasMasPracticadas' }" exact exact-active-class="active">Favoritas</b-nav-item>
-      </b-nav>
-      <!-- Child route gets rendered in <router-view> or <nuxt-child> -->
-        
-      <router-view></router-view>
-        </b-container>
-   
+  <b-container>
+    <b-nav card-header pills>
+      <!-- <b-nav-item>'s with child routes. Note the trailing slash on the first <b-nav-item> -->
+      <b-nav-item to="/" exact-active-class="active">Home</b-nav-item>
+      <b-nav-item :to="{ name: 'About' }" exact-active-class="active"
+        >About</b-nav-item
+      >
+      <b-nav-item :to="{ name: 'Login' }" exact-active-class="active"
+        >Login</b-nav-item
+      >
+      <b-nav-item :to="{ name: 'MisPracticas' }" exact-active-class="active"
+        >Cancionero</b-nav-item
+      >
+      <b-nav-item :to="{ name:'LasMasPracticadas'}" exact-active-class="active"
+        >Favoritas</b-nav-item
+      >
+      <b-nav-item :to="{ name: 'Reproductor' }" exact-active-class="active"
+         >Mi Leccion</b-nav-item
+      >
+    </b-nav>
+    <!-- Child route gets rendered in <router-view> or <nuxt-child> -->
+
+    <router-view></router-view>
+  </b-container>
 </template>
 
 <style>
