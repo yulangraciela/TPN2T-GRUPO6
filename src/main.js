339779d7543c3eab509e9d5b201c625f3211bb1a
graciela yulan <yulan.it.graciela@gmail.com>
2021-06-07 18:38:15 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,11 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 //import Router from 'vue-router';
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
 
 ///Vue.use(Router)
 // Make BootstrapVue available throughout your project
@@ -18,7 +18,7 @@ Vue.use(IconsPlugin);
 Vue.config.productionTip = false;
 
 new Vue({
-    router,
-    store,
-    render: (h) => h(App),
+  router,
+  store,
+  render: (h) => h(App),
 }).$mount("#app");
